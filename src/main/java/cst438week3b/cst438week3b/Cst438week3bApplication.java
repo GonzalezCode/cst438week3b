ea21c59aee8c871953a817b75d4dcf3ce8cbafd4
gonzalezcode <marcus68@protonmail.com>
2020-07-13 19:47:17 -0700

    Initial commit. Complete

---
+++ src/main/java/cst438week3b/cst438week3b/Cst438week3bApplication.java
@@ -0,0 +1,13 @@
+package cst438week3b.cst438week3b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438week3bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cst438week3bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cst438week3b/cst438week3b/Cst438week3bApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438week3b.cst438week3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438week3bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
